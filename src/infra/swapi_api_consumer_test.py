27122287348133cf40068e32fb3655abba18427e
Gabriel <gabrielvie25@outlook.com>
2021-12-15 21:48:28 -0300

    test: testing mock http request

--- src/infra/swapi_api_consumer_test.py
+++ src/infra/swapi_api_consumer_test.py
@@ -1,7 +1,9 @@
 from .swapi_api_consumer import SwapApiConsumer
 
-def test_get_starship():
+def test_get_starship(requests_mock):
     ''' Testing get_startship method '''
+
+    requests_mock.get('https://gorest.co.in/public/v1/users', status_code=200, json={ 'some': 'thing' })
     swapi_api_consumer = SwapApiConsumer()
     response = swapi_api_consumer.get_starships(page=1)
 
