7e647752c455c067b9bb7fb5e0ff18238920eef3
Gabriel <gabrielvie25@outlook.com>
2021-12-19 18:02:22 -0300

    feat: Implementing error in swapi_api_consumer and testing with mocks

--- src/infra/swapi_api_consumer.py
+++ src/infra/swapi_api_consumer.py
@@ -1,10 +1,52 @@
+from collections import namedtuple
+from typing import Tuple, Type, Dict
 import requests
+from requests import Request
+from src.errors import HttpRequestError
 
-class SwapApiConsumer:
+class SwapiApiConsumer:
+    ''' Class to consume swapi api with http requests '''
+
+    def __init__(self) -> None:
+        self.get_starships_response = namedtuple('GET_Starships', 'status_code request response')
+
+    def get_starships(self, page: int) -> Tuple[int, Type[Request], Dict]:
+        '''
+            Request starships in pagination
+            :param - page: int with page of navegation
+            :return - Tuple with status_code, request, response attributes
+        '''
+
+        req = requests.Request(
+            method='GET',
+            url='https://swapi.dev/api/starships/',
+            params={'page': page}
+        )
+
+        req_prepared = req.prepare()
+        response = self.__send_http_request(req_prepared)
+        status_code = response.status_code
+
+        if status_code >= 200 and status_code <= 299:
+            return self.get_starships_response(
+                status_code=status_code,
+                request=req,
+                response=response.json()
+            )
+        else:
+            raise HttpRequestError(
+                message=response.json()['detail'],
+                status_code=status_code
+            )
 
     @classmethod
-    def get_starships(self, page: int) -> any:
-        params = {'page': page}
-        response = requests.get('https://gorest.co.in/public/v1/users', params=params)
-        
-        return response.json()
+    def __send_http_request(cls, req_prepared: Type[Request]) -> any:
+        ''' Prepare a session and send a http request
+            :param - req_prepared: Request object with all params
+            :response - Http response raw
+        '''
+
+        http_session = requests.Session()
+        response = http_session.send(req_prepared)
+
+        return response
--- src/infra/swapi_api_consumer_test.py
+++ src/infra/swapi_api_consumer_test.py
@@ -1,10 +1,43 @@
-from .swapi_api_consumer import SwapApiConsumer
+from .swapi_api_consumer import SwapiApiConsumer
+from src.errors import HttpRequestError
 
-def test_get_starship(requests_mock):
-    ''' Testing get_startship method '''
+def test_get_starships(requests_mock):
+    ''' Testing get_startships method '''
 
-    requests_mock.get('https://gorest.co.in/public/v1/users', status_code=200, json={ 'some': 'thing' })
-    swapi_api_consumer = SwapApiConsumer()
-    response = swapi_api_consumer.get_starships(page=1)
+    requests_mock.get(
+        'https://swapi.dev/api/starships/',
+        status_code=200,
+        json={ 'some': 'thing', 'results': [{}] }
+        )
+    swapi_api_consumer = SwapiApiConsumer()
+    page = 1
 
-    print(response)
+    get_starship_response = swapi_api_consumer.get_starships(page=page)
+
+    assert get_starship_response.request.method == 'GET'
+    assert get_starship_response.request.url == 'https://swapi.dev/api/starships/'
+    assert get_starship_response.request.params == {'page': page}
+
+    assert get_starship_response.status_code == 200
+    assert isinstance(get_starship_response.response['results'], list)
+
+
+def test_get_starships_http_error(requests_mock):
+    ''' Testing error in get_starships method '''
+
+    requests_mock.get(
+        'https://swapi.dev/api/starships/',
+        status_code=404,
+        json={ 'detail': 'something' }
+        )
+    swapi_api_consumer = SwapiApiConsumer()
+    page = 100
+
+    try:
+        swapi_api_consumer.get_starships(page=page)
+        # se não ocorrer erro, força um erro dizendo que o método não está gerando
+        # um erro proposital
+        assert True is False
+    except HttpRequestError as error:
+        assert error.message is not None
+        assert error.status_code is not None
