1fa8cc2df310a12ef985964d753d284a093a9a14
Gabriel <gabrielvie25@outlook.com>
2022-01-11 21:49:15 -0300

    feat: Implementing usecases of list starships

--- src/data/usecases/starships_list_collect_test.py
+++ src/data/usecases/starships_list_collect_test.py
@@ -1,10 +1,15 @@
-from src.domain.usecases import starships_list_collector
+from src.infra.test.swapi_api_consumer import SwapiApiConsumerSpy
 from .starships_list_collector import StarshipsListCollector
-from src.infra import SwapiApiConsumer
 
 def test_list():
-    api_consumer = SwapiApiConsumer()
+    ''' testing list method '''
+    api_consumer = SwapiApiConsumerSpy()
     starships_list_collector = StarshipsListCollector(api_consumer)
 
     page = 1
-    starships_list_collector.list(page)
+    response = starships_list_collector.list(page)
+
+    assert api_consumer.get_starships_attributes == { 'page': page }
+    assert isinstance(response, list)
+    assert "id" in response[0]
+    assert "MGLT" in response[0]
